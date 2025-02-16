76588308b17fa4b27a3e3cf5857d3b4dddf5f82b
sajedul4D <sajedultasin17@gmail.com>
2025-02-17 04:00:16 +0600

    added same tailwind

--- src/Account.jsx
+++ src/Account.jsx
@@ -6,7 +6,7 @@ const Account = () => {
         <>
          <div className='w-11/12 mx-auto  bg-gradient-to-bl to-green-100'>
            <div className='w-1/6 mx-auto border-b'>
-           <img className='w-20 h-20 ml-5' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzYFFI2bX0ck3fpWTavZ91fRh7ZhFNqVxvJg&s" alt="" />
+           <img className='w-20 h-20 ml-12' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzYFFI2bX0ck3fpWTavZ91fRh7ZhFNqVxvJg&s" alt="" />
 
            </div>
            <div className='flex  justify-center items-center mt-10 '>
